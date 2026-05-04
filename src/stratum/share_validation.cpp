d139077cf529394cb7e38a5bfc12deef57b3fbf7
Sam <43705650+samsam380@users.noreply.github.com>
2026-05-04 22:41:28 +0700

    stratum: use non-witness coinbase serialization for V1 shares

--- src/stratum/share_validation.cpp
+++ src/stratum/share_validation.cpp
@@ -3,10 +3,12 @@
 // file COPYING or https://opensource.org/license/mit/.
 
 #include <stratum/share_validation.h>
+#include <consensus/merkle.h>
 #include <hash.h>
-#include <util/strencodings.h>
-#include <crypto/sha256.h>
+#include <primitives/transaction.h>
 #include <stratum/jobmanager.h>
+#include <stratum/server.h>
+#include <streams.h>
 #include <util/strencodings.h>
 
 #include <algorithm>
@@ -56,9 +58,11 @@ arith_uint256 GetShareTarget(double difficulty)
 }
 } // namespace
 
-ShareValidationResult ValidateShare(const SubmitRequest& req, const Session& session, const Job& job)
+ShareValidationResult ValidateShare(const SubmitRequest& req, const Session& session, const Job& job, const Config& config)
 {
     ShareValidationResult ret;
+    ret.job_version = job.version;
+    ret.version_rolling_mask = config.version_rolling_mask;
     if (req.extranonce2.size() != session.extranonce2_size * 2) {
         ret.reject_reason = "invalid-extranonce2-size";
         return ret;
@@ -69,14 +73,27 @@ ShareValidationResult ValidateShare(const SubmitRequest& req, const Session& ses
         return ret;
     }
 
-    CBlockHeader header;
-    header.nVersion = static_cast<int32_t>(job.version);
+    uint32_t final_version = job.version;
     if (req.version_bits.has_value()) {
         if (!IsHex(req.version_bits.value())) {
             ret.reject_reason = "invalid-version-bits";
             return ret;
         }
+        if (!config.version_rolling) {
+            ret.reject_reason = "version-rolling-not-enabled";
+            return ret;
+        }
+        const uint32_t submitted_version = ParseHexU32(req.version_bits.value());
+        ret.submitted_version_bits = submitted_version;
+        if ((submitted_version & ~config.version_rolling_mask) != 0) {
+            ret.reject_reason = "invalid-version-bits-mask";
+            return ret;
+        }
+        final_version = (job.version & ~config.version_rolling_mask) | (submitted_version & config.version_rolling_mask);
     }
+
+    CBlockHeader header;
+    header.nVersion = static_cast<int32_t>(final_version);
 
     header.hashPrevBlock = job.prevhash;
     header.nBits = job.nbits;
@@ -88,7 +105,21 @@ ShareValidationResult ValidateShare(const SubmitRequest& req, const Session& ses
         return ret;
     }
 
-    uint256 merkle = job.block.vtx.at(0)->GetHash();
+    CMutableTransaction coinbase;
+    std::vector<unsigned char> coinbase_raw;
+    try {
+        coinbase_raw = ParseHex(job.coinb1 + session.extranonce1 + req.extranonce2 + job.coinb2);
+        DataStream ds{coinbase_raw};
+        ds >> TX_NO_WITNESS(coinbase);
+    } catch (...) {
+        ret.reject_reason = "invalid-coinbase";
+        return ret;
+    }
+    ret.coinbase_raw_size = coinbase_raw.size();
+    const size_t prefix_len = std::min<size_t>(coinbase_raw.size(), 32);
+    ret.coinbase_raw_prefix = HexStr(coinbase_raw.begin(), coinbase_raw.begin() + prefix_len);
+
+    uint256 merkle = Hash(coinbase_raw);
     for (const auto& h : job.merkle_branches) {
         merkle = HashConcat(merkle, h);
     }
@@ -107,6 +138,12 @@ ShareValidationResult ValidateShare(const SubmitRequest& req, const Session& ses
     ret.accepted_share = hash_val <= share_target;
     ret.accepted_block = hash_val <= network_target;
     ret.block_hash = header.GetHash();
+    ret.final_version = final_version;
+    ret.coinbase_hash = Hash(coinbase_raw);
+    ret.merkle_root = merkle;
+    DataStream header_stream;
+    header_stream << header;
+    ret.header_hex = HexStr(header_stream);
     ret.share_target = share_target;
     ret.network_target = network_target;
     if (!ret.accepted_share) ret.reject_reason = "low-difficulty-share";
--- src/stratum/share_validation.h
+++ src/stratum/share_validation.h
@@ -16,6 +16,7 @@
 namespace stratum {
 
 struct Job;
+struct Config;
 
 struct ShareValidationResult {
     bool accepted_share{false};
@@ -24,9 +25,18 @@ struct ShareValidationResult {
     uint256 block_hash;
     arith_uint256 share_target;
     arith_uint256 network_target;
+    uint32_t job_version{0};
+    uint32_t submitted_version_bits{0};
+    uint32_t version_rolling_mask{0};
+    uint32_t final_version{0};
+    uint256 coinbase_hash;
+    uint256 merkle_root;
+    std::string coinbase_raw_prefix;
+    size_t coinbase_raw_size{0};
+    std::string header_hex;
 };
 
-ShareValidationResult ValidateShare(const SubmitRequest& req, const Session& session, const Job& job);
+ShareValidationResult ValidateShare(const SubmitRequest& req, const Session& session, const Job& job, const Config& config);
 
 } // namespace stratum
 
